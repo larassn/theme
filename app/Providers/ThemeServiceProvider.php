4f8cfc7a1890434fea492a51a8691e58d9270058
Ali Sasani <a_S47_1@yahoo.com>
2024-10-06 23:00:53 +0330

    [init] 🚀 init module

---
+++ app/Providers/ThemeServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Theme\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
---
+++ tests/ThemeServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Theme\Tests;
+
+class ThemeServiceProviderTest extends \Tests\TestCase
+{
+	// TODO
+}
